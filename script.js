52c00938a87dec670df9fd15b92ff06b29e5f428
Lev <minaev.leva@yandex.ru>
2024-03-21 23:23:16 +0500

    24 lesson PRACTICE JS

--- script.js
+++ script.js
@@ -199,7 +199,7 @@
 // console.log(4 + ' - object');
 
 // let incr = 10,
-    // decr = 10;
+// decr = 10;
 
 // incr++;
 // ++incr;
@@ -239,7 +239,7 @@
 ///// 18 /////
 
 
-///// 19 /////
+///// 19 ///// PRACTICE /////
 
 
 ////////// 1
@@ -260,7 +260,7 @@
 // };
 
 
-////////// 3 
+////////// 3
 
 
 // const a = prompt('Один из последних просмотренных фильмов?', ''),
@@ -449,7 +449,7 @@
 // const length = 7;
 
 // for (let i = 1; i < length; i++) {
-    
+
 //     for (let j = 0; j < i; j++) {
 //         result += '*';
 //     }
@@ -475,5 +475,42 @@
 // }
 
 
-///// 24 /////
+///// 24 ///// PRACTICE /////
 
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+//     personalMovieDB.movies[a] = b;
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно много фильмов');   
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Проищошла ошибка');
+// }
+
+// console.log(personalMovieDB);
