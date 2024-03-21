60c3ee4fa67a3beab96f4d2835540c59efc8d4c3
Lev <minaev.leva@yandex.ru>
2024-03-21 15:53:33 +0500

    21 lesson JS

--- script.js
+++ script.js
@@ -203,30 +203,76 @@ let incr = 10,
 // 1
 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
 // 2
 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
 // 3 
 
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
 
-    console.log(personalMovieDB);
+//     console.log(personalMovieDB);
+
+
+// 20 //
+
+
+// if (4 == 9) {
+//     console.log('ok');
+// } else {
+//     console.log('error');
+// }
+
+//
+
+// const num = 1000;
+
+// if (num < 49) {
+//     console.log('error');
+// } else if (num > 100) {
+//     console.log('too much');
+// } else {
+//     console.log('ok');
+// }
+
+// (num === 50) ? console.log('ok') : console.log('error');
+
+
+//
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('error');
+//         break;
+//     case 100:
+//         console.log('error');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default:
+//         console.log('not found');
+//         break;
+// }
+
+// 21 //
