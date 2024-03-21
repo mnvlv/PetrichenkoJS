3944fd1ed2050dcbdbe65708bc09881d21c29cf2
Lev <minaev.leva@yandex.ru>
2024-03-21 15:35:48 +0500

    FIRST PRACTICE JS

--- script.js
+++ script.js
@@ -196,3 +196,37 @@ let incr = 10,
 // Работа с Git
 
 // 18 //
+
+// 19 //
+
+
+// 1
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// 2
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+// 3 
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
