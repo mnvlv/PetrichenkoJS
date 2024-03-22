7182ad7d5776b8044a7c00de3998b7dc1a8cd3c9
Lev <minaev.leva@yandex.ru>
2024-03-22 15:51:16 +0500

    27 lesson JS

--- script.js
+++ script.js
@@ -514,3 +514,109 @@
 // }
 
 // console.log(personalMovieDB);
+
+
+///// 25 /////
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     // let num = 40;
+//     console.log(num);
+// }
+
+// showFirstMessage('hello wrld');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 10));
+// console.log(calc(3, 2));
+// console.log(calc(5, 5));
+
+
+//////////
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('hello');
+// };
+
+// logger();
+
+
+//////////
+
+
+// const calc = (a, b) => { return a + b };
+// const calc = (a, b) => a + b;
+
+
+///// 26 /////
+
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert (amount, curr) {
+//     console.log(curr * amount);
+// }
+
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+
+///// 27 /////
+
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500 , usdCurr);
+// promotion(res);
+
+
+//////////
+
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('done');
+// }
+
+// test();
+
+
+//////////
+
+
+// function doNothing() {};
+// console.log(doNothing() === undefined);
+
+
+///// 28 /////
+
+
