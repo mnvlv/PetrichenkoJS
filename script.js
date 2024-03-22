c20e1769bbabe398db8f4100279d1aba2633d6c1
Lev <minaev.leva@yandex.ru>
2024-03-22 17:23:57 +0500

    28 lesson JS

--- script.js
+++ script.js
@@ -620,3 +620,66 @@
 ///// 28 /////
 
 
+// const str = "test",
+//     arr = [1, 2, 3];
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+
+
+//////////
+
+
+// const str = 'test';
+
+// console.log(str);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('fruit'));
+
+
+/////
+
+
+// const logg = 'hello wrld';
+
+// console.log(logg.slice(3, 5));
+
+
+/////
+
+
+// console.log(logg.substring(7, 2))
+
+
+/////
+
+
+// console.log(logg.substr(6, 3));
+
+
+/////
+
+
+// const num = 12.2;
+
+// console.log(Math.round(num));
+
+
+/////
+
+
+// const test = '12.2px';
+
+// console.log(parseInt(test));
+
+// console.log(parseFloat(test));
+
+
+///// 29 /////
+
+
